4af3a546363a875519a8c89393e7f7bb3ec81827
shyatxun <157414760+shyatxun@users.noreply.github.com>
2024-02-21 13:29:55 -0800

    Create 608. Tree Node.sql

---
+++ Medium/608. Tree Node.sql
@@ -0,0 +1,3 @@
+# https://leetcode.com/problems/tree-node/description/
+
+select id, case when p_id is null then 'Root' when id in (select distinct p_id from tree) then 'Inner' else 'Leaf' end as type from tree
