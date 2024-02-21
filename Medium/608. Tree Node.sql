b4104a0dbcd5964874fff6be7eb488013d937731
shyatxun <157414760+shyatxun@users.noreply.github.com>
2024-02-21 13:36:02 -0800

    Update 608. Tree Node.sql

--- Medium/608. Tree Node.sql
+++ Medium/608. Tree Node.sql
@@ -1,3 +1,4 @@
 # https://leetcode.com/problems/tree-node/description/
 
 select id, case when p_id is null then 'Root' when id in (select distinct p_id from tree) then 'Inner' else 'Leaf' end as type from tree
+# *** null in pid, when a value is compared to a null value, neither 'True' or 'False' is returned, but rather 'Unknown' is returned -> dont use (id not in (select distinct p_id from tree) then 'Leaf')
